2befc877f3786a11533a8156b032b04fd380a17f
BlaiseGratton <blaisegratton@gmail.com>
2015-10-06 15:54:22 -0500

    binding between checklist and map sort of working

--- main.js
+++ main.js
@@ -2,17 +2,55 @@
 
 var app = angular.module('countyMap', ['datamaps']);
 
+app.controller('mainController', function($scope) {
+  $scope.counties = [
+    {
+      'id': 1,
+      'name': 'Davidson',
+      'region': 3
+    }, {
+      'id': 2,
+      'name': 'Coffee',
+      'region': 2
+    }, {
+      'id': 3,
+      'name': 'Williamson',
+      'region': 1
+    }, {
+      'id': 4,
+      'name': 'Knox',
+      'region': 4
+    }
+  ];
+
+  $scope.selectedCounties = [];
+
+  $scope.toggleCounty = function(county) {
+    var idx = $scope.selectedCounties.indexOf(county);
+    if (idx < 0) 
+      $scope.selectedCounties.push(county);
+    else 
+      $scope.selectedCounties.splice(idx, 1);
+  };
+});
+
 app.controller('mapController', function($scope) {
 
-  var vm = this;
-
-  vm.tennessee = {
+  $scope.tennessee = {
     scope: 'tn_counties',
     geographyConfig: {
-      dataUrl: '/topo_tn_counties.json'
+      dataUrl: '/topo_tn_counties.json',
+      highlightOnHover: false
     },
-    mapOptions: {
+    mapOptions: {},
+    options: {
+      staticGeoData: true
     },
+    fills: {
+      'selected': '#a55',
+      'defaultFill': '#cdcdcd'
+    },
+    data: {},
     setProjection: function(element, options) {
       var offsetWidth = -100;
       var offsetHeight = 100;
@@ -32,10 +70,44 @@ app.controller('mapController', function($scope) {
     }
   };
 
-  vm.selectedCounties = [];
+  $scope.$watch('selectedCounties', function(newVal, oldVal) {
+    var newNames = _.pluck(newVal, $scope.key);
+    var oldNames = _.pluck(oldVal, $scope.key);
+    var longerCollection = newNames.length > oldNames.length ? newNames : oldNames;
+    var shorterCollection = oldNames.length < newNames.length ? oldNames : newNames;
+    var difference = _.difference(longerCollection, shorterCollection);
+    $scope.selectCounty(difference[0]);
+  }, true);
 
-  vm.selectCounty = function(geography) {
-    vm.selectedCounties.push(geography.properties);
+  $scope.selectCounty = function(countyName) {
+    var idx = $scope.selectedCounties.indexOf(countyName);
+    if (idx === -1) {
+      $scope.tennessee.data[countyName] = { 'fillKey': 'selected' };
+    } else {
+      $scope.tennessee.data[countyName] = { 'fillKey': 'defaultFill' };
+    }
+  };
+
+  $scope.clickCounty = function(geography) {
+    $scope.selectCounty(geography.id);
+    var idx = $scope.selectedCounties.indexOf(geography.id);
+    if (idx > -1) 
+      $scope.selectedCounties.splice(idx, 1);
+    else 
+      $scope.selectedCounties.push(geography.id);
     $scope.$apply();
   }
+});
+
+app.directive('countyMap', function() {
+  return {
+    restrict: 'EA',
+    controller: 'mapController',
+    template: '<datamap map="tennessee"' +
+                 'on-click="clickCounty"></datamap>',
+    scope: {
+      'selectedCounties': '=',
+      'key': '@'
+    }
+  };
 });
