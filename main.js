c2c8faae5d1719eecc03d747ee13330baabaaabf
BlaiseGratton <blaisegratton@gmail.com>
2015-11-04 16:38:03 -0600

    merge changes to main.js into master

--- main.js
+++ main.js
@@ -19,14 +19,17 @@
       options: {
         staticGeoData: true
       },
-      fills: {},
+      fills: {
+        selected: '#a55',
+        defaultFill: '#9cf'
+      },
       data: {},
       setProjection: function(element, options) {
         var svg = element.childNodes[0];
         svg.attributes.width.value = '849';
         svg.attributes.height.value = '238';
         svg.setAttribute('viewBox', '377 44 800 190');
-        if (!!$scope.options.classes) {
+        if (!!$scope.options && !!$scope.options.classes) {
           $scope.options.classes.forEach(function(className){
             svg.classList.add(className);
           });
@@ -36,10 +39,10 @@
         var offsetHeight = 100;
 
         var projection = d3.geo.mercator()
-            .center([-86.7833, 36.1667])
-            .scale(5000)
-            .rotate([8, 0])
-            .translate([0, 100]);
+          .center([-86.7833, 36.1667])
+          .scale(5000)
+          .rotate([8, 0])
+          .translate([0, 100]);
 
         var path = d3.geo.path().projection(projection);
 
@@ -50,35 +53,52 @@
       }
     };
 
-    if(!!$scope.options.fills) {
+    if (!!$scope.options && !!$scope.options.fills) {
       $scope.tennessee.fills.selected = $scope.options.fills.selected || '#a55';
       $scope.tennessee.fills.defaultFill = $scope.options.fills.defaultFill || '#99ccff';
     }
 
-
-
     //WATCHES CHANGES TO selectedCounties AND CALLS INTO COLOR COUNTY METHOD.
     var deregister = $scope.$watch('selectedCounties', function(newVal, oldVal) {
-      colorCounties(newVal);
+      $scope.colorCounties(newVal);
     }, true);
 
-    function colorCounties(counties) {
-      if(!counties) return;
 
-      var selectedCountyNames = _.map(counties, function(county) {
-        return county[$scope.key].countyMapsToLowerNoSpaces();
-      });
+    if($scope.options && $scope.options.multiple) {
 
-      $scope.tennessee.data = _.mapObject($scope.tennessee.data, function(countyName) {
-        if(selectedCountyNames.indexOf(countyName) < 0) {
+      $scope.colorCounties = function(counties) {
+        if(!counties) return;
+
+        var selectedCountyNames = _.map(counties, function(county) {
+          return county[$scope.key].countyMapsToLowerNoSpaces();
+        });
+
+        $scope.tennessee.data = _.mapObject($scope.tennessee.data, function(countyName) {
+          if(selectedCountyNames.indexOf(countyName) < 0) {
+            return { 'fillKey' : 'defaultFill' };
+          }
+        });
+
+        selectedCountyNames.forEach(function(countyName){
+          $scope.tennessee.data[countyName] = { 'fillKey' : 'selected' };
+        });
+      };
+    } else {
+      $scope.colorCounties = function(county) {
+        $scope.tennessee.data = _.mapObject($scope.tennessee.data, function(countyName) {
           return { 'fillKey' : 'defaultFill' };
-        }
-      });
+        });
 
-      selectedCountyNames.forEach(function(countyName){
-        $scope.tennessee.data[countyName] = { 'fillKey' : 'selected' };
-      });
-    };
+        if(!county || !Object.keys(county).length) 
+          return;
+
+        var selectedCountyName = county[$scope.key].countyMapsToLowerNoSpaces();
+        
+        if ($scope.selectedCounties.length !== 0)
+          $scope.tennessee.data[selectedCountyName] = { 'fillKey' : 'selected' };
+      };
+
+    }
 
     $scope.$on('$destroy', deregister);
 
@@ -104,21 +124,24 @@
           $scope.options.onAfterCountySelect(clickedCounty, geography);
         }
       }
-
     } else {
       $scope.toggleCounty = function(geography) {
         var clickedCounty = _.find($scope.counties, function(county) {
           return county[$scope.key].countyMapsToLowerNoSpaces() === geography.id.countyMapsToLowerNoSpaces();
         });
-
-        $scope.selectedCounties = [clickedCounty];
+        
+        if ($scope.selectedCounties === clickedCounty) {
+          $scope.selectedCounties = null;
+        } else {
+          $scope.selectedCounties = clickedCounty;
+        }
         $scope.$apply();
 
-        if(!!$scope.options.onAfterCountySelect) {
+        if(!!$scope.options && !!$scope.options.onAfterCountySelect) {
           $scope.options.onAfterCountySelect(clickedCounty, geography);
         }
       }
     }
-
   }]);
 }());
+
